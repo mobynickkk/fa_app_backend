7d56535f0c0a3ae21e2219d1026ec791e3f61817
mobynick <mobynick365@gmail.com>
2020-09-06 23:31:19 +0300

    API urls integrated in main urls file

--- fa_app_back/urls.py
+++ fa_app_back/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from restapi import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(urls))
 ]
