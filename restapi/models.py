75cb49105ad329ea0b8ea186d4869218b32934b0
mobynick <mobynick365@gmail.com>
2020-09-06 22:38:27 +0300

    Profile class fields completed

--- restapi/models.py
+++ restapi/models.py
@@ -4,4 +4,12 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # TODO
+    photo = models.ImageField()
+    name = models.CharField(max_length=128)
+    faculty = models.CharField(max_length=32)
+    group = models.CharField(max_length=8)
+    is_monitor = models.BooleanField(default=False)
+
+
+class HomeTask(models.Model):
+    pass
