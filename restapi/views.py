f3f1888639e8af367b3d9252837da649dba431d4
mobynick <mobynick365@gmail.com>
2020-09-06 23:24:15 +0300

    HomeTaskAPI added

--- restapi/views.py
+++ restapi/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets, views
+from rest_framework import viewsets, views, mixins, generics
 from rest_framework.response import Response
-from .models import Profile, Subject, HomeTask
-from .serializers import ProfileSerializer
+from .models import Profile, Subject, HomeTask, Group
+from .serializers import ProfileSerializer, HomeTaskSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -15,3 +15,17 @@ class ProfileByEmail(views.APIView):
         profile = Profile.objects.get(user=User.objects.get(username=request.GET['email']))
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+
+class HomeTaskAPI(mixins.CreateModelMixin, mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = HomeTask.objects.all()
+    serializer_class = HomeTaskSerializer
+
+    def get(self, request, *args, **kwargs):
+        self.queryset = HomeTask.objects.filter(
+            func=lambda task: task.group == Group.objects.get(pk=request.GET["group"])
+        )
+        return self.retrieve(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
