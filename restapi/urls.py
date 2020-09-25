87e215b839be0c6e6fee8e92ef8afb715fcd060f
mobynick <mobynick365@gmail.com>
2020-09-25 18:06:26 +0300

    Added route for schedule

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import ProfileViewSet, ProfileByEmail, HomeTaskAPI
+from .views import ProfileViewSet, ProfileByNameAndGroup, HomeTaskAPI, get_calendar
 
 router = routers.DefaultRouter()
 router.register(r'users', ProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('profile/', ProfileByEmail.as_view()),
-    path('hometask/', HomeTaskAPI.as_view())
+    path('profile/', ProfileByNameAndGroup.as_view()),
+    path('hometask/', HomeTaskAPI.as_view()),
+    path('schedule/', get_calendar)
 ]
