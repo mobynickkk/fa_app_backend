9b83c542871a3fae0307840ab52ec5a856b10946
mobynick <mobynick365@gmail.com>
2020-09-26 09:55:43 +0300

    Serializers updated

--- restapi/serializers.py
+++ restapi/serializers.py
@@ -1,23 +1,13 @@
-from .models import Profile, Subject, HomeTask
+from .models import Profile, HomeTask
 from rest_framework import serializers
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(read_only=True, source="user.first_name")
-    last_name = serializers.CharField(read_only=True, source="user.last_name")
-    email = serializers.CharField(read_only=True, source="user.email")
     group = serializers.CharField(read_only=True, source="group.index")
 
     class Meta:
         model = Profile
-        fields = ['id', 'first_name', 'last_name', 'email', 'group']
-
-
-class SubjectSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Subject
-        exclude = []
+        fields = ['id', 'name', 'group']
 
 
 class HomeTaskSerializer(serializers.ModelSerializer):
