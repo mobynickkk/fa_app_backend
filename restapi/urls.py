84e4916bc4a66dd38daaa17c324d85b7143815ff
mobynick <mobynick365@gmail.com>
2020-09-26 10:58:11 +0300

    Urls updated after tests

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import ProfileViewSet, ProfileByNameAndGroup, HomeTaskAPI, get_calendar
+from .views import ProfileViewSet, ProfileByNameAndGroup, HomeTaskAPI, get_calendar, HomeTaskByProfile
 
 router = routers.DefaultRouter()
 router.register(r'users', ProfileViewSet)
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('profile/', ProfileByNameAndGroup.as_view()),
     path('hometask/', HomeTaskAPI.as_view()),
-    path('schedule/', get_calendar)
+    path('schedule/', get_calendar),
+    path('homework/', HomeTaskByProfile.as_view())
 ]
