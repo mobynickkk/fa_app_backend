fdb47f07074534ad3c5100040b7737bc7145f3ab
mobynick <mobynick365@gmail.com>
2020-09-26 10:55:44 +0300

    Models redeveloped after tests

--- restapi/models.py
+++ restapi/models.py
@@ -5,7 +5,7 @@ class Group(models.Model):
     field = models.CharField(max_length=2)
     first_year = models.IntegerField()
     number = models.IntegerField()
-    index = models.CharField(primary_key=True, max_length=6)
+    index = models.CharField(blank=True, primary_key=True, max_length=6)
 
     def __str__(self):
         return self.index
@@ -13,13 +13,19 @@ class Group(models.Model):
     def save(self, *args, **kwargs):
         field = self.field.upper()
         self.index = field+str(self.first_year)+'-'+str(self.number)
-        super().save(*args, **kwargs)
+        super(Group, self).save(*args, **kwargs)
 
 
 class Profile(models.Model):
     name = models.CharField(max_length=128)
     group = models.ForeignKey(Group, null=True, on_delete=models.SET_NULL)
     is_monitor = models.BooleanField(default=False)
+    hash = models.CharField(max_length=64, blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        from hashlib import sha256
+        self.hash = sha256((self.name+self.group.index).encode()).hexdigest()
+        super(Profile, self).save(*args, **kwargs)
 
 
 class HomeTask(models.Model):
