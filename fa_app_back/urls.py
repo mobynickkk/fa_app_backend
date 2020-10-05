8e89e34d6322ac263bcf61ce3250d8046b61bc0a
mobynick <mobynick365@gmail.com>
2020-10-05 17:53:58 +0300

    Added csrf exempt

--- fa_app_back/urls.py
+++ fa_app_back/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 from restapi import urls as rest_urls
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(rest_urls)),
-    path('api/v2/', GraphQLView.as_view(graphiql=True))
+    path('api/v2/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
